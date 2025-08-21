2bc1da8501671d81e6200b77298ff1eaf120e962
Monodip16 <dasmonodip108@gmail.com>
2025-08-21 21:58:03 +0000

    variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const account_id = 24516
+let account_email = "Monodip872@gmail.com"
+var account_password = "706362"
+account_city = "Kolkata"
+
+
+//account_id = 2 // not allowed
+account_email ="mono234@gmail.com"
+account_city ="mumbai"
+let account_state;
+
+//console.log(account_email)
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([account_id, account_email, account_password, account_city, account_state])
