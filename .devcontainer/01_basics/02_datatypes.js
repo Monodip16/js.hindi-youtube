5d389bb870223ab1ae2dabafb71d51efac9ec4e2
Monodip16 <dasmonodip108@gmail.com>
2025-08-21 21:58:26 +0000

    datatyoes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all js code as newer version
+
+let name = "Monodip Das"
+let age = 21
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object 
+
+console.log(typeof "Mono")
+console.log(typeof null)
