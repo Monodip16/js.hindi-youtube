e76c409126b856d1f3096154e405165086cd76cc
Monodip16 <dasmonodip108@gmail.com>
2025-08-22 18:04:49 +0000

    Conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,13 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueNumber = Number(score)
+console.log(typeof valueNumber)
+
+let number = 33
+
+let stringNumber = String(number)
+console.log(stringNumber)
+console.log(typeof(stringNumber))
